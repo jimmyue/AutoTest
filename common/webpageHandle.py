71c73298b64080241a2985bed3dd0f7861dd4f5f
jimmyue <jimmy_job@126.com>
2019-10-08 15:26:46 +0800

    封装UI方法

--- common/webpageHandle.py
+++ common/webpageHandle.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.support.expected_conditions import NoSuchElementException
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 
@@ -8,124 +8,22 @@ class webHandle:
 	def __init__(self,driver):
 		self.driver=driver
 
-	def find_element_located(self,method,element):
+	def findElement(self,*loc):
 		"""封装id、name、xpath、link_text、class、css定位方法"""
-		"""显性等待元素是否出现，再对网页元素进行定位操作"""
-		if str.upper(method)=='ID':
-			try:
-				locator = (By.ID,element)
-				ele=WebDriverWait(self.driver, 10, 0.5).until(EC.presence_of_element_located(locator))
-				return ele
-			except:
-				print("can't find '%s' through %s" % (element,method))
-				self.driver.quit()
-		elif str.upper(method)=='NAME':
-			try:
-				locator = (By.NAME,element)
-				ele=WebDriverWait(self.driver, 10, 0.5).until(EC.presence_of_element_located(locator))
-				return ele
-			except:
-				print("can't find '%s' through %s" % (element,method))
-				self.driver.quit()
-		elif str.upper(method)=='XPATH':
-			try:
-				locator = (By.XPATH,element)
-				ele=WebDriverWait(self.driver, 10, 0.5).until(EC.presence_of_element_located(locator))
-				return ele
-			except:
-				print("can't find '%s' through %s" % (element,method))
-				self.driver.quit()
-		elif str.upper(method)=='LINK_TEXT':
-			try:
-				locator = (By.LINK_TEXT,element)
-				ele=WebDriverWait(self.driver, 10, 0.5).until(EC.presence_of_element_located(locator))
-				return ele
-			except:
-				print("can't find '%s' through %s" % (element,method))
-				self.driver.quit()
-		elif str.upper(method)=='CLASS':
-			try:
-				locator = (By.CLASS_NAME,element)
-				ele=WebDriverWait(self.driver, 10, 0.5).until(EC.presence_of_element_located(locator))
-				return ele
-			except:
-				print("can't find '%s' through %s" % (element,method))
-				self.driver.quit()
-		elif str.upper(method)=='CSS':
-			try:
-				locator = (By.CSS_SELECTOR,element)
-				ele=WebDriverWait(self.driver, 10, 0.5).until(EC.presence_of_element_located(locator))
-				return ele
-			except:
-				print("can't find '%s' through %s" % (element,method))
-				self.driver.quit()
-		else:
-			print('该元素定位方法未定义！')
+		"""显性等待单个元素是否出现，再对网页元素进行定位操作"""
+		try:
+			return WebDriverWait(self.driver,20).until(lambda x:x.find_element(*loc))
+		except NoSuchElementException as e:
+			print('Error Details {0}'.format(e.args[0]))
 
-	def find_element_alert(self,method,element):
+	def findsElement(self,*loc):
 		"""封装id、name、xpath、link_text、class、css定位方法"""
-		"""显性等待元素是否可见，再对网页元素进行定位操作"""
-		if str.upper(method)=='ID':
-			try:
-				locator = (By.ID,element)
-				ele=WebDriverWait(self.driver, 10, 0.5).until(EC.alert_is_present(locator))
-				return ele
-			except:
-				print("can't find '%s' through %s" % (element,method))
-				self.driver.quit()
-		elif str.upper(method)=='NAME':
-			try:
-				locator = (By.NAME,element)
-				ele=WebDriverWait(self.driver, 10, 0.5).until(EC.alert_is_present(locator))
-				return ele
-			except:
-				print("can't find '%s' through %s" % (element,method))
-				self.driver.quit()
-		elif str.upper(method)=='XPATH':
-			try:
-				locator = (By.XPATH,element)
-				ele=WebDriverWait(self.driver, 10, 0.5).until(EC.alert_is_present(locator))
-				return ele
-			except:
-				print("can't find '%s' through %s" % (element,method))
-				self.driver.quit()
-		elif str.upper(method)=='LINK_TEXT':
-			try:
-				locator = (By.LINK_TEXT,element)
-				ele=WebDriverWait(self.driver, 10, 0.5).until(EC.alert_is_present(locator))
-				return ele
-			except:
-				print("can't find '%s' through %s" % (element,method))
-				self.driver.quit()
-		elif str.upper(method)=='CLASS':
-			try:
-				locator = (By.CLASS_NAME,element)
-				ele=WebDriverWait(self.driver, 10, 0.5).until(EC.alert_is_present(locator))
-				return ele
-			except:
-				print("can't find '%s' through %s" % (element,method))
-				self.driver.quit()
-		elif str.upper(method)=='CSS':
-			try:
-				locator = (By.CSS_SELECTOR,element)
-				ele=WebDriverWait(self.driver, 10, 0.5).until(EC.alert_is_present(locator))
-				return ele
-			except:
-				print("can't find '%s' through %s" % (element,method))
-				self.driver.quit()
-		else:
-			print('该元素定位方法未定义！')
+		"""显性等待多个元素是否出现，再对网页元素进行定位操作"""
+		try:
+			return WebDriverWait(self.driver,20).until(lambda x:x.find_elements(*loc))
+		except NoSuchElementException as e:
+			print('Error Details {0}'.format(e.args[0]))
 
-	def title(self):
-		return self.driver.title
-
-	def open_browser(self,url):
-		self.driver.maximize_window()
-		self.driver.implicitly_wait(30) #隐形等待,设置页面超时时间
-		self.driver.get(url)
-
-	def close_browser(self):
-		self.driver.quit()
 
 
 
